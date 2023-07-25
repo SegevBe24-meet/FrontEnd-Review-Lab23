15e1190eb92e236be6188207fa2413e470cbe717
SegevBe24-meet <139688996+SegevBe24-meet@users.noreply.github.com>
2023-07-25 17:41:52 +0300

    s

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function changeBackgroundColor(color) {
 	 document.body.style.backgroundColor = color;
 }
+ function changeColor(color) {
+   document.getElementById("cool").style.text-color="lightblue";
+ }
